80cc02ea4f9d033278bab3fac6b75557d7b21ab7
dillic <8921364+dillic@users.noreply.github.com>
2024-10-24 19:32:43 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -196,3 +196,265 @@ $Wrapper = {
     . $GlobalState {. $Deferred; Remove-Variable Deferred}
 }
 $null = $Powershell.AddScript($Wrapper.ToString()).BeginInvoke()
+
+function Edit-Profile {
+    code $PROFILE.CurrentUserAllHosts
+}
+function touch($file) { "" | Out-File $file -Encoding ASCII }
+function ff($name) {
+    Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
+        Write-Output "$($_.FullName)"
+    }
+}
+
+# Network Utilities
+function Get-PubIP { (Invoke-WebRequest http://ifconfig.me/ip).Content }
+
+# Open WinUtil
+function winutil {
+	iwr -useb https://christitus.com/win | iex
+}
+
+# System Utilities
+function admin {
+    if ($args.Count -gt 0) {
+        $argList = "& '$args'"
+        Start-Process wt -Verb runAs -ArgumentList "pwsh.exe -NoExit -Command $argList"
+    } else {
+        Start-Process wt -Verb runAs
+    }
+}
+
+# Set UNIX-like aliases for the admin command, so sudo <command> will run the command with elevated rights.
+Set-Alias -Name su -Value admin
+
+function uptime {
+    if ($PSVersionTable.PSVersion.Major -eq 5) {
+        Get-WmiObject win32_operatingsystem | Select-Object @{Name='LastBootUpTime'; Expression={$_.ConverttoDateTime($_.lastbootuptime)}} | Format-Table -HideTableHeaders
+    } else {
+        net statistics workstation | Select-String "since" | ForEach-Object { $_.ToString().Replace('Statistics since ', '') }
+    }
+}
+
+function reload-profile {
+    & $profile
+}
+
+function unzip ($file) {
+    Write-Output("Extracting", $file, "to", $pwd)
+    $fullFile = Get-ChildItem -Path $pwd -Filter $file | ForEach-Object { $_.FullName }
+    Expand-Archive -Path $fullFile -DestinationPath $pwd
+}
+
+function grep($regex, $dir) {
+    if ( $dir ) {
+        Get-ChildItem $dir | select-string $regex
+        return
+    }
+    $input | select-string $regex
+}
+
+function df {
+    get-volume
+}
+
+function sed($file, $find, $replace) {
+    (Get-Content $file).replace("$find", $replace) | Set-Content $file
+}
+
+function which($name) {
+    Get-Command $name | Select-Object -ExpandProperty Definition
+}
+
+function export($name, $value) {
+    set-item -force -path "env:$name" -value $value;
+}
+
+function pkill($name) {
+    Get-Process $name -ErrorAction SilentlyContinue | Stop-Process
+}
+
+function pgrep($name) {
+    Get-Process $name
+}
+
+function head {
+  param($Path, $n = 10)
+  Get-Content $Path -Head $n
+}
+
+function tail {
+  param($Path, $n = 10, [switch]$f = $false)
+  Get-Content $Path -Tail $n -Wait:$f
+}
+
+# Quick File Creation
+function nf { param($name) New-Item -ItemType "file" -Path . -Name $name }
+
+# Directory Management
+function mkcd { param($dir) mkdir $dir -Force; Set-Location $dir }
+
+### Quality of Life Aliases
+
+# Navigation Shortcuts
+function docs { Set-Location -Path $HOME\Documents }
+
+function dtop { Set-Location -Path $HOME\Desktop }
+
+# Quick Access to Editing the Profile
+function ep { vim $PROFILE }
+
+# Simplified Process Management
+function k9 { Stop-Process -Name $args[0] }
+
+# Enhanced Listing
+function la { Get-ChildItem -Path . -Force | Format-Table -AutoSize }
+function ll { Get-ChildItem -Path . -Force -Hidden | Format-Table -AutoSize }
+
+# Git Shortcuts
+function gs { git status }
+
+function ga { git add . }
+
+function gc { param($m) git commit -m "$m" }
+
+function gp { git push }
+
+function g { __zoxide_z github }
+
+function gcl { git clone "$args" }
+
+function gcom {
+    git add .
+    git commit -m "$args"
+}
+function lazyg {
+    git add .
+    git commit -m "$args"
+    git push
+}
+
+# Quick Access to System Information
+function sysinfo { Get-ComputerInfo }
+
+# Networking Utilities
+function flushdns {
+	Clear-DnsClientCache
+	Write-Host "DNS has been flushed"
+}
+
+# Clipboard Utilities
+function cpy { Set-Clipboard $args[0] }
+
+function pst { Get-Clipboard }
+
+# Enhanced PowerShell Experience
+Set-PSReadLineOption -Colors @{
+    Command = 'Yellow'
+    Parameter = 'Green'
+    String = 'DarkCyan'
+}
+
+$PSROptions = @{
+    ContinuationPrompt = '  '
+    Colors             = @{
+    Parameter          = $PSStyle.Foreground.Magenta
+    Selection          = $PSStyle.Background.Black
+    InLinePrediction   = $PSStyle.Foreground.BrightYellow + $PSStyle.Background.BrightBlack
+    }
+}
+Set-PSReadLineOption @PSROptions
+Set-PSReadLineKeyHandler -Chord 'Ctrl+f' -Function ForwardWord
+Set-PSReadLineKeyHandler -Chord 'Enter' -Function ValidateAndAcceptLine
+
+$scriptblock = {
+    param($wordToComplete, $commandAst, $cursorPosition)
+    dotnet complete --position $cursorPosition $commandAst.ToString() |
+        ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock $scriptblock
+
+# Help Function
+function Show-Help {
+    @"
+PowerShell Profile Help
+=======================
+
+Update-PowerShell - Checks for the latest PowerShell release and updates if a new version is available.
+
+Edit-Profile - Opens the current user's profile for editing using the configured editor.
+
+touch <file> - Creates a new empty file.
+
+Get-PubIP - Retrieves the public IP address of the machine.
+
+winutil - Runs the WinUtil script from Chris Titus Tech.
+
+uptime - Displays the system uptime.
+
+reload-profile - Reloads the current user's PowerShell profile.
+
+unzip <file> - Extracts a zip file to the current directory.
+
+df - Displays information about volumes.
+
+grep <regex> [dir] - Searches for a regex pattern in files within the specified directory or from the pipeline input.
+
+sed <file> <find> <replace> - Replaces text in a file.
+
+which <name> - Shows the path of the command.
+
+export <name> <value> - Sets an environment variable.
+
+pkill <name> - Kills processes by name.
+
+pgrep <name> - Lists processes by name.
+
+head <path> [n] - Displays the first n lines of a file (default 10).
+
+tail <path> [n] - Displays the last n lines of a file (default 10).
+
+nf <name> - Creates a new file with the specified name.
+
+mkcd <dir> - Creates and changes to a new directory.
+
+docs - Changes the current directory to the user's Documents folder.
+
+dtop - Changes the current directory to the user's Desktop folder.
+
+ep - Opens the profile for editing.
+
+k9 <name> - Kills a process by name.
+
+la - Lists all files in the current directory with detailed formatting.
+
+ll - Lists all files, including hidden, in the current directory with detailed formatting.
+
+gs - Shortcut for 'git status'.
+
+ga - Shortcut for 'git add .'.
+
+gc <message> - Shortcut for 'git commit -m'.
+
+gp - Shortcut for 'git push'.
+
+g - Changes to the GitHub directory.
+
+gcom <message> - Adds all changes and commits with the specified message.
+
+lazyg <message> - Adds all changes, commits with the specified message, and pushes to the remote repository.
+
+sysinfo - Displays detailed system information.
+
+flushdns - Clears the DNS cache.
+
+cpy <text> - Copies the specified text to the clipboard.
+
+pst - Retrieves text from the clipboard.
+
+Use 'Show-Help' to display this help message.
+"@
+}
+Write-Host "Use 'Show-Help' to display help"
